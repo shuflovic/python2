5b2fe26f5b6cb5c6d1c7dc5c1e9941bce11a8f75
shuflovic <pavel.skuta@gmail.com>
2025-05-05 15:02:33 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const clientId = '41d6acedbfa44710b2183b1b01e495f6'; // Replace with your actual Client ID
+const clientId = 'my id'; // Replace with your actual Client ID
 const clientSecret = 'secret is missing; // Replace with your actual Client Secret
 const redirectUri = 'file:///home/chronos/u-31a2f0582bdcd36fd29b650681cd562640bf2681/MyFiles/webka/games/spotify.html'; // Replace with your actual Redirect URI
 
