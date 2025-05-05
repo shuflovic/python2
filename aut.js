7f7d3dc7d7f7594b084c3cabbd5e659148156abc
shuflovic <pavel.skuta@gmail.com>
2025-05-05 19:51:10 +0700

    Update aut.js

--- aut.js
+++ aut.js
@@ -4,7 +4,7 @@ let clientSecret;
 document.addEventListener('DOMContentLoaded', async () => {
   // Initialize Supabase client
   const SUPABASE_URL = 'https://rigsljqkzlnemypqjlbk.supabase.co';
-  const SUPABASE_KEY = 'YOUR_SUPABASE_KEY';
+  const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpZ3NsanFremxuZW15cHFqbGJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NjI5NTUsImV4cCI6MjA2MTIzODk1NX0.hNdNu9fHGQfdh4WdMFx_SQAVjXvQutBIud3D5CkM9uY';
   const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
   async function getSpotifyCredentials() {
